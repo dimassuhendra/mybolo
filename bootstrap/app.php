bd72cbb08fda224b7672d388586de9acab4c3553
Dimas Suhendra <dimassuhendra0104@gmail.com>
2026-05-10 13:20:05 +0700

    update middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,19 +6,21 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Gabungkan semua konfigurasi middleware di sini
         $middleware->web(append: [
             \App\Http\Middleware\LogUserActivity::class,
         ]);
+
+        // Jika ada alias, tambahkan di sini juga
+        // $middleware->alias([
+        //    'role' => \App\Http\Middleware\RoleMiddleware::class,
+        // ]);
     })
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-    
